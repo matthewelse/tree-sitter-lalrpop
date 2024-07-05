a58ce4ea82156030dcb0cec9f1c7ba98b18c5386
Matthew Else <matthewelse1997@gmail.com>
2024-07-05 18:58:44 +0100

    import implementation of bsearch to make wasm work

--- src/scanner.c
+++ src/scanner.c
@@ -6,6 +6,28 @@
 #include <stdlib.h>
 #include <wctype.h>
 
+void *
+bsearch (const void *key, const void *base0,
+         size_t nmemb, size_t size,
+         int (*compar)(const void *, const void *))
+{
+	const char *base = (const char *) base0;
+	int lim, cmp;
+	const void *p;
+
+	for (lim = nmemb; lim != 0; lim >>= 1) {
+		p = base + (lim >> 1) * size;
+		cmp = (*compar)(key, p);
+		if (cmp == 0)
+			return (void *)p;
+		if (cmp > 0) {	/* key > p: move right */
+			base = (const char *)p + size;
+			lim--;
+		} /* else move left */
+	}
+	return (NULL);
+}
+
 #define SYMBOLS(...)                                                           \
 	enum TokenType { __VA_ARGS__ };                                            \
 	const enum TokenType variants[] = { __VA_ARGS__ };
